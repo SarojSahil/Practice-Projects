225eae7745e054f3d2bf81a9bf5e5b95338393cf
SarojSahil <120626766+SarojSahil@users.noreply.github.com>
2026-06-30 22:56:16 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/Practice-Projects/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src")
